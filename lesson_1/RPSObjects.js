13f73152c7bacd9d3584b742ad3d01142fd85240
Caleb Smith <smithcaleb042@gmail.com>
2020-12-11 16:33:16 +0000

    add spock and lizard to RPS

--- lesson_1/RPSObjects.js
+++ lesson_1/RPSObjects.js
@@ -3,18 +3,31 @@ const readline = require("readline-sync");
 function createMove() {
   return {
 
-  }
+  };
 }
 
 function createRule() {
   return {
-    
-  }
+    compare(humanMove, computerMove) {
+      if ((humanMove === 'rock' && (computerMove === 'scissors' || computerMove === 'lizard')) ||
+          (humanMove === 'paper' && (computerMove === 'rock' || computerMove === 'Spock')) ||
+          (humanMove === 'scissors' && (computerMove === 'paper' || computerMove === 'lizard')) ||
+          (humanMove === 'lizard' && (computerMove === 'Spock' || computerMove === 'paper')) ||
+          (humanMove === 'spock' && (computerMove === 'scissors' || computerMove === 'rock'))) {
+        return 1;
+      } else if (humanMove === computerMove) {
+        return 0;
+      } else {
+        return -1;
+      }
+    }
+  };
 }
 
 function createPlayer() {
   return {
     move: null,
+    score: 0
   };
 }
 
@@ -25,7 +38,7 @@ function createComputer() {
     move: null,
 
     choose() {
-      const choices = ['rock', 'paper', 'scissors'];
+      const choices = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
       let randomIndex = Math.floor(Math.random() * choices.length);
       this.move = choices[randomIndex];
     },
@@ -36,25 +49,20 @@ function createComputer() {
 
 function createHuman() {
   let playerObject = createPlayer();
-  
+
   let humanObject = {
     choose() {
-      if (this.isHuman()) {
-        let choice;
+      let choice;
 
-        while (true) {
-          console.log('Please choose rock, paper, or scissors:');
-          choice = readline.question();
-          if (['rock', 'paper', 'scissors'].includes(choice)) break;
-          console.log('Sorry, invalid choice');
-        }
-
-        this.move = choice;
-      } else {
-        const choices = ['rock', 'paper', 'scissors'];
-        let randomIndex = Math.floor(Math.random() * choices.length);
-        this.move = choices[randomIndex]; 
+      while (true) {
+        console.log('Please choose rock, paper, scissors, lizard, or spock:');
+        choice = readline.question();
+        if (['rock', 'paper', 'scissors', 'spock', 'lizard'].includes(choice)) break;
+        console.log('Sorry, invalid choice');
       }
+
+      this.move = choice;
+
     },
   };
 
@@ -64,6 +72,7 @@ function createHuman() {
 const RPSGame = {
   human: createHuman(),
   computer: createComputer(),
+  rule: createRule(),
 
   displayWelcomeMessage() {
     console.log('Welcome to Rock, Paper, Scissors!');
@@ -78,19 +87,25 @@ const RPSGame = {
     let computerMove = this.computer.move;
 
     console.log(`You chose: ${this.human.move}`);
-    console.log(`The computer chose ${this.computer.move}`);
-    
-    if ((humanMove === 'rock' && computerMove === 'scissors') ||
-        (humanMove === 'paper' && computerMove === 'rock') ||
-        (humanMove === 'scissors' && computerMove === 'paper')) {
+    console.log(`The computer chose: ${this.computer.move}`);
+
+    if (this.rule.compare(humanMove, computerMove) === 1) {
       console.log('You win!');
-    } else if ((humanMove === 'rock' && computerMove === 'paper') ||
-               (humanMove === 'paper' && computerMove === 'scissors') ||
-               (humanMove === 'scissors' && computerMove === 'rock')) {
+      this.human.score += 1;
+    } else if (this.rule.compare(humanMove, computerMove) === -1) {
       console.log('Computer wins!');
+      this.computer.score += 1;
     } else {
       console.log("It's a tie!");
     }
+  },
+  
+  displayScore() {
+    console.log(`Human Score: ${this.human.score} Computer score: ${this.computer.score}`);
+  },
+  
+  displayChampion() {
+    console.log(`The grand champion is ${this.human.score === 5 ? 'Human!' : 'Computer!'}`);
   },
 
   playAgain() {
@@ -100,11 +115,18 @@ const RPSGame = {
   },
 
   play() {
-    this.displayWelcomeMessage();
     while (true) {
-      this.human.choose();
-      this.computer.choose();
-      this.displayWinner();
+      this.displayWelcomeMessage();
+      while (true) {
+        this.human.choose();
+        this.computer.choose();
+        this.displayWinner();
+        this.displayScore();
+        if (this.computer.score === 5 || this.human.score === 5) {
+          this.displayChampion();
+          break;
+        }
+      }
       if (!this.playAgain()) break;
     }
 
