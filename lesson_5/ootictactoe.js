eeb053394aa8bdbb8cc4159c1ca7dba13713753e
Caleb Smith <smithcaleb042@gmail.com>
2020-12-22 15:38:52 -0700

    assignment 7 tic tac toe

--- lesson_5/ootictactoe.js
+++ lesson_5/ootictactoe.js
@@ -20,6 +20,10 @@ class Square {
   setMarker(marker) {
     this.marker = marker;
   }
+
+  getMarker() {
+    return this.marker;
+  }
 }
 
 class Board {
@@ -28,6 +32,18 @@ class Board {
     for (let counter = 1; counter <= 9; ++counter) {
       this.squares[String(counter)] = new Square();
     }
+  }
+
+  countMarkersFor(player, keys) {
+    let markers = keys.filter(key => {
+      return this.squares[key].getMarker() === player.getMarker();
+    });
+
+    return markers.length;
+  }
+
+  isFull() {
+    this.unusedSquares().length === 0;
   }
 
   unusedSquares() {
@@ -53,12 +69,6 @@ class Board {
     console.log(`  ${this.squares["7"]}  |  ${this.squares["8"]}  |  ${this.squares["9"]}`);
     console.log("     |     |");
     console.log("");
-  }
-}
-
-class Row {
-  constructor() {
-    
   }
 }
 
@@ -89,6 +99,17 @@ class Computer extends Player {
 }
 
 class TTTGame {
+  static POSSIBLE_WINNING_ROWS = [
+    [ "1", "2", "3" ],            // top row of board
+    [ "4", "5", "6" ],            // center row of board
+    [ "7", "8", "9" ],            // bottom row of board
+    [ "1", "4", "7" ],            // left column of board
+    [ "2", "5", "8" ],            // middle column of board
+    [ "3", "6", "9" ],            // right column of board
+    [ "1", "5", "9" ],            // diagonal: top-left to bottom-right
+    [ "3", "5", "7" ],            // diagonal: bottom-left to top-right
+  ];
+
   constructor() {
     this.board = new Board();
     this.human = new Human();
@@ -96,22 +117,23 @@ class TTTGame {
   }
   
   play() {
-    //SPIKE
     this.displayWelcomeMessage();
+    
     
     while (true) {
       this.board.display();
-      
+
       this.humanMoves();
-      //this.board.display();
       if (this.gameOver()) break;
       
       this.computerMoves();
-      //this.board.display();
       if (this.gameOver()) break;
-      //break;
+
+      console.clear();
     }
     
+    console.clear();
+    this.board.display();
     this.displayResults();
     this.displayGoodbyeMessage();
   }
@@ -125,9 +147,20 @@ class TTTGame {
   }
   
   displayResults() {
-    
+    if (this.isWinner(this.human)) {
+      console.log('You won! Congratulations!');
+    } else if (this.isWinner(this.computer)) {
+      console.log('I won! I won! Take that, human!');
+    } else {
+      console.log("It's a tie game. How boring.");
+    }
   }
-  
+
+  isWinner(player) {
+    return TTTGame.POSSIBLE_WINNING_ROWS.some(row => {
+      return this.board.countMarkersFor(player, row) === 3;
+    });
+  }
   
   humanMoves() {
     let choice;
@@ -158,8 +191,11 @@ class TTTGame {
   }
   
   gameOver() {
-    
-    return false;
+    return this.board.isFull() || this.someoneWon();
+  }
+
+  someoneWon() {
+    return this.isWinner(this.human) || this.isWinner(this.computer);
   }
 }
 
