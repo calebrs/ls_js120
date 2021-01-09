e94301725544e3184a816ee59f38105be73ca4d3
Caleb Smith <smithcaleb042@gmail.com>
2021-01-09 11:31:07 -0700

    assessment commit

--- testprep/temp.js
+++ testprep/temp.js
@@ -1,25 +1,4 @@
-let Person = class {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+let obj1 = { foo: 1, bar: 2 };
+let obj2 = Object.assign(obj1, { baz: 3 });
 
-  sayName() {
-    console.log(`My name is ${this.name}.`);
-  }
-};
-
-let Student = class extends Person {
-  constructor(name, age, semester) {
-    super(name, age);
-    this.semester = semester;
-  }
-
-  enrollInCourse(courseNumber) {
-    console.log(`${this.name} has enrolled in course ${courseNumber}.`);
-  }
-};
-
-let student = new Student('Kim', 22, 'Fall');
-student.sayName();
-student.enrollInCourse('JS120');
+console.log(obj2.hasOwnProperty('foo'));
