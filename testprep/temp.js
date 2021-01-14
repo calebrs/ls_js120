5a09027411d3984c44f254086d9b78a0ef8df59d
Caleb Smith <smithcaleb042@gmail.com>
2021-01-14 11:47:08 -0700

    fix merge

--- testprep/temp.js
+++ testprep/temp.js
@@ -1,4 +1,56 @@
-let obj1 = { foo: 1, bar: 2 };
-let obj2 = Object.assign(obj1, { baz: 3 });
+// translate to a OLOO
+let personPrototype = {
+  people: [],
+  
+  init(name, age) {
+    this.name = name;
+    this.age = age;
+    personPrototype.people.push(this);
+    
+    return this;
+  },
+  
+  introduce() {
+    console.log(`Hello, I am ${this.name}`);
+  }
+}
 
-console.log(obj2.hasOwnProperty('foo'));
+let ellie = Object.create(personPrototype).init('ellie', 26);
+let caleb2 = Object.create(personPrototype).init('caleb', 25);
+// console.log(ellie.constructor.name); //global object
+
+// console.log(personPrototype.introduce());
+
+function Organism(name) {
+  this.name = name;
+}
+
+Organism.thing = 10;
+
+Organism.prototype.sayName = function() {
+  console.log(`I am ${this.name}`);
+}
+
+Organism.sayThing = function() {
+  console.log(this.thing);
+}
+
+
+
+class Organism2 {
+  static importantNumber = 10;
+  
+  constructor(name) {
+    this.name = name
+  }
+  
+  sayName() {
+    console.log(`I am ${this.name}`);
+  }
+  
+  static sayImportantNumber() {
+    console.log(this.importantNumber);
+  }
+}
+
+Organism.sayThing();
